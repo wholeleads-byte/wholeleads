0b3bdaeb8b53b5a7686028af7c13de8f9583afef
wholeleads-byte <wholeleads@gmail.com>
2026-01-09 14:25:33 +0530

    Update wholeleadsmain.js

--- wholeleadsmain.js
+++ wholeleadsmain.js
@@ -1,26 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const saved = localStorage.getItem("siteSettings");
-  if (!saved) return;
 
-  const data = JSON.parse(saved);
+  let data = {
+    logo: "/MAINLOGO/Whole-leads-main-logo.png",
+    email: "support@wholeleads.com",
+    phone: "+91 7814007003",
+    address: "Santa Clara, US"
+  };
+
+  if (saved) {
+    data = { ...data, ...JSON.parse(saved) };
+  }
 
   /* LOGO */
   const logo = document.getElementById("siteLogo");
-  if (logo && data.logo) {
-    logo.src = data.logo;
-  }
+  if (logo) logo.src = data.logo;
 
-  /* FOOTER EMAIL */
+  /* EMAIL */
   const footerEmail = document.getElementById("footerEmail");
-  if (footerEmail && data.email) {
-    footerEmail.innerText = data.email;
-  }
+  if (footerEmail) footerEmail.innerText = data.email;
 
-  /* FOOTER PHONE */
+  /* PHONE */
   const footerPhone = document.getElementById("footerPhone");
-  if (footerPhone && data.phone) {
-    footerPhone.innerText = data.phone;
-  }
+  if (footerPhone) footerPhone.innerText = data.phone;
+
+  /* ADDRESS */
+  const footerAddress = document.getElementById("footerAddress");
+  if (footerAddress) footerAddress.innerText = data.address;
 
 });
+
